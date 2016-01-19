e7931fce101517f5b87819a383c3ef5056f0480e
Gareth Tan <garethwtan@gmail.com>
2016-01-18 19:41:09 -0500

    Update for 0.16.

--- DynamicStyle.elm
+++ DynamicStyle.elm
@@ -1,7 +1,9 @@
-module DynamicStyle where
+module DynamicStyle (..) where
 
 {-|
 # Library for simple, dynamic style effects
+
+@docs hover, hover', pressure, pressure', focus, focus', cssStateEffect, CSSKey, CSSValue, JSEventAttribute
 
 The goal of this library is make locally stateful, declarative CSS effects
 (e.g., :hover, :focus, etc.) as easy in Elm as they are with stylesheets.
@@ -30,70 +32,102 @@ an event, you can simply make a wrapper element and hook its events.
 
 -}
 
-import Html(Attribute)
-import Html.Attributes(style,attribute)
-import String(toUpper,toLower,left,dropLeft,join,split)
-import List(map,foldl)
+import Html exposing (Attribute)
+import Html.Attributes exposing (style, attribute)
+import String exposing (toUpper, toLower, left, dropLeft, join, split)
+import List exposing (map, foldl)
 
-type alias CSSKey = String
-type alias CSSValue = String
+{-|
+The key for a CSS property.
+-}
+type alias CSSKey =
+    String
 
-type alias JSEventAttribute = String
+{-|
+The value of a CSS property.
+-}
+type alias CSSValue =
+    String
+
+{-|
+THe name of a JavaScript event attribute.
+-}
+type alias JSEventAttribute =
+    String
+
 
 {-|
 Change styles when the user hovers over an element. For example,
 hover [("color","black","blue")] will render black text normally, but
 blue text when the user hovers over the element.
 -}
-hover : List (CSSKey,CSSValue,CSSValue) -> List Attribute
-hover = hover' []
+hover : List ( CSSKey, CSSValue, CSSValue ) -> List Attribute
+hover =
+    hover' []
+
 
 {-|
 Shorthand to add a list of static base styles.
 -}
-hover' : List (CSSKey,CSSValue) -> List (CSSKey,CSSValue,CSSValue) -> List Attribute
-hover' = cssStateEffect ["onmouseout"] "onmouseover"
+hover' : List ( CSSKey, CSSValue ) -> List ( CSSKey, CSSValue, CSSValue ) -> List Attribute
+hover' =
+    cssStateEffect [ "onmouseout" ] "onmouseover"
+
 
 {-|
 Change styles when the user pushes on element. For example,
 pressure [("color","black","blue")] will render black text normally, but
 blue text when the user pushes the mouse down on the element.
 -}
-pressure : List (CSSKey,CSSValue,CSSValue) -> List Attribute
-pressure = pressure' []
+pressure : List ( CSSKey, CSSValue, CSSValue ) -> List Attribute
+pressure =
+    pressure' []
+
 
 {-|
 Shorthand to add a list of static base styles.
 -}
-pressure' : List (CSSKey,CSSValue) -> List (CSSKey,CSSValue,CSSValue) -> List Attribute
-pressure' = cssStateEffect ["onmouseup","onmouseout"] "onmousedown"
+pressure' : List ( CSSKey, CSSValue ) -> List ( CSSKey, CSSValue, CSSValue ) -> List Attribute
+pressure' =
+    cssStateEffect [ "onmouseup", "onmouseout" ] "onmousedown"
+
 
 {-|
 Change styles when the user focuses on element. For example,
 pressure [("border-color","black","blue")] will render a black border
 normally, but a blue border when the user focuses on the element.
 -}
-focus : List (CSSKey,CSSValue,CSSValue) -> List Attribute
-focus = focus' []
+focus : List ( CSSKey, CSSValue, CSSValue ) -> List Attribute
+focus =
+    focus' []
+
 
 {-|
 Shorthand to add a list of static base styles.
 -}
-focus' : List (CSSKey,CSSValue) -> List (CSSKey,CSSValue,CSSValue) -> List Attribute
-focus' = cssStateEffect ["onblur"] "onfocus"
+focus' : List ( CSSKey, CSSValue ) -> List ( CSSKey, CSSValue, CSSValue ) -> List Attribute
+focus' =
+    cssStateEffect [ "onblur" ] "onfocus"
+
 
 {-|
 Construct your own stateful effects by providing a list of JavaScript hooks
 to indicate an inactive state, a hook to indicate the active state, static
 styles, and a tuple-map for your dynamic styles.
 -}
-cssStateEffect : List JSEventAttribute -> JSEventAttribute -> List (CSSKey,CSSValue) -> List (CSSKey,CSSValue,CSSValue) -> List Attribute
+cssStateEffect : List JSEventAttribute -> JSEventAttribute -> List ( CSSKey, CSSValue ) -> List ( CSSKey, CSSValue, CSSValue ) -> List Attribute
 cssStateEffect jsEventInactives jsEventActive baseState hoverState =
-    let jsName cssAttr = (\s -> toLower (left 1 s) ++ dropLeft 1 s)
-        <| join ""
-        <| map (\s -> toUpper (left 1 s) ++ dropLeft 1 s)
-        <| split "-" cssAttr in
-    let toJS attrs = foldl (\(a,b) x -> x ++ "this.style." ++ jsName a ++ "='"++b++"';") "" attrs in
-    [ style (baseState ++ map (\(a,b,_) -> (a,b)) hoverState)
-    , attribute jsEventActive <| toJS <| map (\(a,_,c) -> (a,c)) hoverState]
-    ++ map (\inactive -> attribute inactive <| toJS <| map (\(a,b,_) -> (a,b)) hoverState) jsEventInactives
+    let
+        jsName cssAttr =
+            (\s -> toLower (left 1 s) ++ dropLeft 1 s)
+                <| join ""
+                <| map (\s -> toUpper (left 1 s) ++ dropLeft 1 s)
+                <| split "-" cssAttr
+    in
+        let
+            toJS attrs = foldl (\( a, b ) x -> x ++ "this.style." ++ jsName a ++ "='" ++ b ++ "';") "" attrs
+        in
+            [ style (baseState ++ map (\( a, b, _ ) -> ( a, b )) hoverState)
+            , attribute jsEventActive <| toJS <| map (\( a, _, c ) -> ( a, c )) hoverState
+            ]
+                ++ map (\inactive -> attribute inactive <| toJS <| map (\( a, b, _ ) -> ( a, b )) hoverState) jsEventInactives
